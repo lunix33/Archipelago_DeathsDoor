cfd5ec540ec7202ac0f6223cde77fa0369499b48
Patrick Bergeron <pbergeron92@gmail.com>
2025-03-01 18:39:37 -0500

    Basic implementation for locations.

---
+++ worlds/deaths_door/location.py
@@ -0,0 +1,38 @@
+from typing import Any, Self
+
+from BaseClasses import Location, Region
+
+from .abc import Data, Idable
+from .extract import RuleJsonSerializer, TermDefinition
+
+class LocationData(Data, Idable):
+    data_file = "locations.json"
+
+    name: str
+    term: str
+
+    def __init__(self, term: str):
+        self.name = term.replace("_", " ")
+        self.term = term
+
+    def to_game_location(self, player: int, region: Region) -> "GameLocation":
+        return GameLocation(self, player, region)
+
+    @classmethod
+    def from_dict(cls, dict: dict[Any, Any]) -> Self:
+        definition = RuleJsonSerializer.from_dict(dict)
+        if not isinstance(definition, TermDefinition):
+            return definition
+        return cls(definition.term)
+
+    @classmethod
+    def get_data(cls) -> list[Self]:
+        return cls.load_get()
+
+class GameLocation(Location):
+    game: str = "Death's Door"
+    data: LocationData
+
+    def __init__(self, data: LocationData, player: int, parent = None):
+        self.data = data
+        super().__init__(player, data.name, data.id(), parent)
